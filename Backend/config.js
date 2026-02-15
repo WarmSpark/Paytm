b09cee52a7024ef2a455fa6f27cf3e9dfdf0ca70
Divyansh Arora <divyansh82188@gmail.com>
2026-02-15 20:27:18 +0530

    step 3

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+
+const JWT_Secret="Divyansh"
+module.exports=JWT_Secret
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,48 @@
 const express=require("express");
-
+const jwt=require("jsonwebtoken")
+const zod=require("zod");
+const {User}=require("../db")
 const Router=express.Router();
- 
-module.export=Router;
+const JWT_SECRET=require("../config")
+
+const SignupSchema=zod.object({
+    username:zod.string(),
+    firstName:zod.string(),
+    lastName:zod.string(),
+    email:zod.string(),
+    password:zod.string()
+})
+Router.post("/signup",async(req,res)=>{
+    const body=req.body;
+    const {success}=SignupSchema.safeParse(req.body);
+    if(!success){
+        return res.json({
+            message:"invalid"
+        })
+    }
+    const user=await User.findOne({
+        username:body.username
+    })
+    if(user){
+        return res.json({
+            message:"user is already there"
+        })
+    }
+    const dbUser=await User.create(body);
+    const token=jwt.sign({
+        userId:dbUser._id
+    },JWT_SECRET)
+
+    res.json({
+        message:"the user is created",
+        token:token
+    })
+})
+
+
+
+
+
+
+
+module.exports=Router;
