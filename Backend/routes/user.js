d9e77806ec2e884421089676a2ed5739d8577b76
Divyansh Arora <divyansh82188@gmail.com>
2026-02-15 21:52:52 +0530

    S

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -4,6 +4,7 @@ const zod=require("zod");
 const {User}=require("../db")
 const Router=express.Router();
 const JWT_SECRET=require("../config")
+const authMiddleware=require("../middleware");
 
 const SignupSchema=zod.object({
     username:zod.string(),
@@ -38,10 +39,48 @@ Router.post("/signup",async(req,res)=>{
         token:token
     })
 })
+const UpdateBody=zod.object({
+    firstName:zod.string().optional(),
+    lastName:zod.string().optional(),
+    password:zod.string().optional()
+})
+Router.put("/",authMiddleware,async(req,res)=>{
+    const {success}=UpdateBody.safeParse(req.body);
+    if(!success){
+        res.status(400).json({
+            message:"error"
+        })
+    }
+    await User.updateOne(req.body,{
+        _id:req.userId
+    })
+    res.json({
+        message:"updated succesfully"
+    })
+})
+Router.get("/bulk",async(req,res)=>{
+    const filter=req.query.filter || "";
+    const users=await User.find({
+        $or:[{
+            firstName:{
+                "$regex": filter
+            }
+        },{
+            lastName:{
+                "$regex": filter
+            }
+        }]
+    })  
+        res.json({
+            user:users.map(user=>({
+                username:user.username,
+                firstName:user.firstName,
+                lastName:user.lastName,
+                _id: user._id
+            }))
+        })
 
-
-
-
+    })
 
 
 
