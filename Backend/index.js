9f8efb5c6e66f9191d5e1175975100608b2da3c3
Divyansh Arora <divyansh82188@gmail.com>
2026-02-15 16:34:01 +0530

    initial

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose =require("mongoose");
+const cors=require("cors")
+const MainRouter=require("./routes/index")
+mongoose.connect("mongodb+srv://divyansh1410:divyansh2005@cluster0.qrenin7.mongodb.net/")
+
+const {UserModel}=require("./db");
+
+const app=express();
+app.use(cors);
+app.use(express.json());
+app.use("/api/v1",MainRouter)
+
+app.listen(3000);
